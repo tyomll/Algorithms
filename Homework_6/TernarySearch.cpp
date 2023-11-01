b89d1ce04b90096b7938f0768bed6d29227a6838
Vahag <sargsyanvahag6@gmail.com>
2023-11-01 18:55:22 +0400

    Ternary Search

---
+++ Homework_6/TernarySearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int ternarySearch(const std::vector<int>& arr, int target, int start, int end) {
+    if (start <= end) {
+        int middenLeft = start + (end - start) / 3;
+        int middenRight = start + 2 * (end - start) / 3;
+
+        if (arr[middenLeft] == target) {
+            return middenLeft;
+        }
+        if (arr[middenRight] == target) {
+            return middenRight;
+        }
+        if (target < arr[middenLeft]) {
+            return ternarySearch(arr, target, start, middenLeft - 1);
+        } 
+        else if (target > arr[middenRight]) {
+            return ternarySearch(arr, target, middenRight + 1, end);
+        } 
+        else {
+            return ternarySearch(arr, target, middenLeft + 1, middenRight - 1);
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    std::vector<int> arr = {1, 5, 7, 8, 18, 25, 36, 100, 110};
+    int index = ternarySearch(arr, 110, 0, arr.size() - 1);
+    std::cout << "Index = " << index << std::endl;
+    return 0;
+}
