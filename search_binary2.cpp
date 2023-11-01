5a857de076a22e0e0c4095c89cb09172873d8820
a_nyut <anyuta.andrea@gmail.com>
2023-11-01 16:34:28 +0400

    search index

---
+++ search_binary2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+// ---- selection ------
+int  searchGivenNumbersIndex(int *arr, int n, int val){
+     int start = 0, end = n-1;
+     int mid = start + (end-start)/2;
+     do {
+         if(arr[mid]==val){
+        std::cout<<"the number's index is: "<<'\t'<<mid<<std::endl;
+        break;
+        } 
+        if(arr[mid]>val){
+            mid=start+((mid-1)-start)/2;
+         }
+        else {
+            mid=(mid+1)+(end-mid+1)/2;
+         }
+
+     }
+     while(start!=end);
+     return -1;
+}
+
+int  main(){
+int n=12;
+int arr[n] ={-11, -5, 0, 2, 4, 11, 15, 22, 34, 55, 101, 111};
+int  val;
+std::cout<<"enter the number you want to  find:"<<std::endl;
+std::cin>>val;
+searchGivenNumbersIndex(arr, n, val);
+
+    return 0;
+}
