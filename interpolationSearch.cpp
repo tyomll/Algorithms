e07b1a114ce457370db7d28531df83c900eede54
Arman Saghatelyan <arman.saghatelyan.2006@gmail.com>
2023-11-07 21:28:23 +0400

    .gitignore updated

--- interpolationSearch.cpp
+++ interpolationSearch.cpp
@@ -1,83 +1,93 @@
 #include <iostream>
+#include <type_traits>
+#include <stdexcept>
 
-int interpolationSearch(int target, int arr[], int higth, int low = 0){
-    int pos = low + (higth - low) * (target - arr[low]) / (arr[higth] - arr[low]);
-    if (arr[pos] == target) {
-        return pos;
+void clearInputBuffer() {
+    while (std::cin.get() != '\n') {
+        continue;
+    }
+}
+
+int interpolationSearch(int target, int arr[], int high, int low = 0) {
+    while (low <= high && target >= arr[low] && target <= arr[high]) {
+        int pos = low + ((double)(target - arr[low]) / (arr[high] - arr[low])) * (high - low);
+        
+        if (arr[pos] == target) {
+            return pos;
+        }
+
+        if (arr[pos] < target) {
+            low = pos + 1;
+        } 
+
+		else {
+            high = pos - 1;
+        }
     }
 
-    if (arr[pos - 1] == target) {
-        return pos - 1;
-    }
-    
-    if (arr[pos + 1] == target) {
-        return pos + 1;
-    }
-
-    if (target > arr[pos]) {
-        interpolationSearch(target, arr, higth, pos + 1);
-    }
-
-    if (target < arr[pos]) {
-        interpolationSearch(target, arr, pos - 1, low);
-    }
-    
     return -1;
 }
 
 template <typename T>
 void inputArr(int arr[], int size) {
-    for (int i = 0;i < size;i++) {
-        T elem;
-        std::cin >> elem;
-        int type = CheckType(elem);
-        if (type == 0){
-            arr[i] = elem;
-        }
-        else {
-            std::cerr << "Input intagers!!!" << std::endl;
+    for (int i = 0; i < size; i++) {
+        try {
+            T elem;
+            std::cin >> elem;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cerr << "Invalid input. Please enter integers." << std::endl;
+                clearInputBuffer();
+                i--;
+            }
+			else {
+                arr[i] = elem;
+            }
+        } 
+		catch (const std::exception& e) {
+            std::cerr << e.what() << std::endl;
             i--;
         }
     }
 }
 
-template <typename T>
-int CheckType(const T& element) {
-    if (std::is_same<T, int>::value) {
-        return 0;
-    }
-     else {
-        return -1;
-    }
-}
-
-int main (){
+int main() {
     int size;
-    std::cout << "input the length: ";
+    std::cout << "Input the length: ";
     std::cin >> size;
 
-    if (size == 0) {
-        std::cerr << "Array is empty" << std::endl;
+    if (size <= 0) {
+        std::cerr << "Array size must be a positive integer." << std::endl;
         return -1;
     }
+
     int arr[size];
     std::cout << "Input the array: ";
-    inputArr(arr, size);
+    inputArr<int>(arr, size);
 
     int target;
     std::cout << "Input the target: ";
-    std::cin >> target;
+    try {
+        std::cin >> target;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cerr << "Invalid input for the target. Please enter an integer." << std::endl;
+            clearInputBuffer(); 
+            return -1;
+        }
 
+        int result = interpolationSearch(target, arr, size - 1, 0);
 
-    int result =  interpolationSearch(target, arr, size - 1, 0);
-
-    if (result == -1){
-        std::cout << "The element not found in array!!" << std::endl;
+        if (result == -1) {
+            std::cout << "The element was not found in the array." << std::endl;
+        } else {
+            std::cout << "The index of the number is: " << result << std::endl;
+        }
     }
-    else  {
-        std::cout << "The index of the number is: " << result << std::endl;
+	catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
-    
     
     return 0;
 }
+
