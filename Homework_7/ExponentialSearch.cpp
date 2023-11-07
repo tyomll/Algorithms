2392d228e171de49b0e2443c269a4284a09502da
Vahag <sargsyanvahag6@gmail.com>
2023-11-07 17:04:40 +0400

    Exponential search algorithm

---
+++ Homework_7/ExponentialSearch.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <math.h>
+
+int binarySearch(std::vector<int>& arr, int left, int right, int target) {
+    if (left <= right) {
+        int index = (left + right) / 2;
+        if (arr[index] == target) {
+            return index;
+        }
+        else if (target < arr[index]) {
+            return binarySearch(arr, left, index - 1, target);
+        }
+        else if (target > arr[index]) {
+            return binarySearch(arr, index + 1, right, target);
+        }
+    }
+
+    return -1;
+}
+
+int exponentialSearch(std::vector<int>& arr, int target) {
+    if (arr.empty()) {
+        return -1;
+    }
+    
+    int index = 0;
+    int degree = -1;
+    while (index <= arr.size() - 1) {
+        if (arr[index] == target) {
+            return index;
+        }
+        else if (arr[index] < target) {
+            index = std::pow(2, ++degree);
+        }
+        else if (arr[index] > target) {
+            return binarySearch(arr, index / 2, index, target);
+        }
+    }
+
+    return binarySearch(arr, index / 2, arr.size() - 1, target);
+}
+
+std::vector<int> inputVector() {
+    std::string userRow, clearRow;
+    std::vector<int> result;
+    std::cout << "Import an array of integer values separated by spaces: For example: 4 8 9 10\n";
+    std::getline(std::cin, userRow);
+    for (int i = 0; i < userRow.length(); ++i) {
+        if (std::isdigit(userRow[i])) {
+            clearRow += userRow[i];
+        }
+        else {
+            clearRow += " ";
+        }
+    }
+
+    std::istringstream iss(clearRow);
+    int number;
+    while (iss >> number) {
+        result.push_back(number);
+    }
+
+    return result;
+}
+
+void printVector(std::vector<int>& arr) {
+    for (int i = 0; i < arr.size(); ++i) {
+        std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+    }
+}
+
+int main() {
+    std::vector<int> arr = inputVector();
+    int target;
+
+    std::cout << "Enter the element to be searched for, of type integer: ";
+    std::cin >> target;
+
+    int index = exponentialSearch(arr, target);
+
+    if (index == -1) {
+        std::cout << "The element you are looking for does not exist in the array\n";
+    }
+    else {
+        std::cout << "The index of the element to be searched for is " << index << std::endl;
+    }
+
+    return 0;
+}
