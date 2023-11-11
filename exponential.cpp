b95ec5f1322d3c8b337ff49ad8db0fd70cc6fa65
ManeShakhkyan <mane02072004@gmail.com>
2023-11-12 00:55:55 +0400

    Exponential search

---
+++ exponential.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+int binary(std::vector<int> array, int target, int start, int end){
+    
+    if (target < array[0] || target > array[array.size() - 1]){
+        return -1;
+    }
+     
+    int mid = (start + end) / 2;
+
+    if (target < array[mid]){
+        return binary(array, target, start, mid - 1);
+    }
+
+    if (target > array[mid]){
+        return binary(array, target, mid + 1, end);
+    }
+
+    return mid; 
+}
+
+int exponential(std::vector<int> array, int target) {
+    
+    if (target < array[0] || target > array[array.size() - 1]) {
+        return -1;
+    }
+
+    if (target == array[0]) {
+        return 0;
+    }
+
+    int step = 1;
+
+    while(step < array.size()){
+        
+        if(target > array[step]){
+            step *= 2;
+        }
+        
+        if (target < array[step]){
+            return binary(array, target, step/2, step-1);
+        }
+        
+        if (target == array[step]){
+            return step;
+        }
+    }
+
+    return binary(array, target, step/2, array.size() - 1);
+
+}
+
+int main() {
+    std::vector<int> array = {1, 2, 3, 4, 5};
+    int target = 5;
+    int index = exponential(array, target);
+
+    if (index == -1) {
+        std::cout << "Target not found!\n";
+    } else {
+        std::cout << "Index of " << target << " is: " << index << "\n";
+    }
+
+    return 0;
+}
