90b79d6e4088a677d45b4ee157b4b561fe384b80
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-11-14 21:48:45 +0400

    Add files via upload

---
+++ marge_sort.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+void merge(std::vector<int> &arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(std::vector<int> &arr, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+    int middle = left + (right - left) / 2;
+    mergeSort(arr, left, middle);
+    mergeSort(arr, middle + 1, right);
+    merge(arr, left, middle, right);
+}
+
+void printArray(std::vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+    std::cout << "Given array is \n";
+    printArray(arr);
+    mergeSort(arr, 0, arr.size() - 1);
+    std::cout << "\nSorted array is \n";
+    printArray(arr);
+    return 0;
+}
+
