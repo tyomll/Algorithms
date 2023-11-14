6211ed096035b7e27358b4e8ab9f41b1ddf49a02
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-11-14 21:45:17 +0400

    binary_search_algorithm.cpp
    changed

--- binary_search_algorithm.cpp
+++ binary_search_algorithm.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 
-int bin_search(int arr[], int l, int r, int x)
-{
-	while (l <= r) {
-		int m = l + (r - l) / 2;
+int binarySearch(int array[], int left, int right, int target) {
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
 
-		if (arr[m] == x)
-			return m;
+        if (array[middle] == target)
+            return middle;
 
-		if (arr[m] < x)
-			l = m + 1;
+        if (array[middle] < target)
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
 
-		else
-			r = m - 1;
-	}
-
-	return -1;
+    return -1;
 }
 
-int main()
-{
-	int arr[] = { 2, 3, 4, 10, 40, 45 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]) ;
-	int result = bin_search(arr, 0, n - 1, x);
-	
+int main() {
+    int numbers[] = {2, 3, 4, 10, 40, 45};
+    int target = 10;
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int result = binarySearch(numbers, 0, size - 1, target);
+
     if (result == -1)
-		std::cout << "Element is not present " << std::endl;
-    else		 
-        std::cout << "Element is present at index " << result <<std::endl;
-	return 0;
+        std::cout << "Element is not present." << std::endl;
+    else
+        std::cout << "Element is present at index " << result << std::endl;
+
+    return 0;
 }
+
 
