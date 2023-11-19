d263d8a0e19a4b6fed90d89529abc0b33f42946b
ManeShakhkyan <mane02072004@gmail.com>
2023-11-20 00:10:57 +0400

    Merge ranges

---
+++ task_11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+struct range {
+    int start;
+    int end;
+};
+
+std::vector<range> merge_ranges(std::vector<range>& input_ranges) {
+    std::vector<range> merged;
+
+    if (input_ranges.empty()) {
+        return merged;
+    }
+
+    range current_range = input_ranges[0];
+
+    for (size_t i = 1; i < input_ranges.size(); ++i) {
+        if (input_ranges[i].start <= current_range.end) {
+            current_range.end = std::max(current_range.end, input_ranges[i].end);
+        } else {
+            merged.push_back(current_range);
+
+            current_range = input_ranges[i];
+        }
+    }
+
+    merged.push_back(current_range);
+
+    return merged;
+}
+
+int main() {
+    std::vector<range> input_ranges = {{1, 3}, {2, 6}, {8, 13}, {15, 18}};
+    std::vector<range> result = merge_ranges(input_ranges);
+
+    for (const auto& range : result) {
+        std::cout << "[" << range.start << ", " << range.end << "] ";
+    }
+    std::cout << "\n";
+    return 0;
+}
