1f50ad9f2d6f393f8e4a70d9f5ae565ec46ac629
Ero <erik.s.mayilyan@gmail.com>
2023-11-27 18:40:10 +0400

    homework 11

---
+++ homework_11.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+
+void printList(std::list<std::pair<int, int>>& list) {
+    std::cout << "[";
+    for (auto element = list.begin(); element != list.end(); ++element) {
+        std::cout << "[" << element->first << ", " << element->second << "]";
+        if (std::next(element) != list.end()) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
+std::list<std::pair<int, int>> mergeIntervals(const std::list<std::pair<int, int>>& intervals) {
+    if (intervals.empty()) {
+        return intervals;
+    }
+
+    std::list<std::pair<int, int>> result;
+    std::vector<std::pair<int, int>> sortedIntervals(intervals.begin(), intervals.end());
+    for (const auto& interval : sortedIntervals) {
+        if (result.empty() || result.back().second < interval.first) {
+            result.push_back(interval);
+        } else {
+            result.back().second = std::max(result.back().second, interval.second);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    std::list<std::pair<int, int>> intervals = {{1, 4}, {4, 5}};
+    std::list<std::pair<int, int>> result = mergeIntervals(intervals);
+    printList(result);
+
+    return 0;
+}
