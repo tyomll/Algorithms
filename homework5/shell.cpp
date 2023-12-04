d8df5af7af1fbd0c1ac401fd85d090cf1a8c1337
Your name <gxachikyan20@gmail.com>
2023-12-04 16:04:11 +0400

    fixing

---
+++ homework5/shell.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int size) {
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < size; i++) {
+            int temp = arr[i];
+            int j = i;
+
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    shellSort(arr, size);
+
+    cout << "Sorted array: ";
+    printArray(arr, size);
+
+    return 0;
+}
